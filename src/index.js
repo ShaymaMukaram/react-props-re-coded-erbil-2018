6d5e0e8b4205a67df2850058bf8836cfc7790bd2
Shayma Mukaram <shaymamukaram@gmail.com>
2018-09-05 19:25:47 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -12,3 +12,13 @@ class MovieCard extends React.Component {
     );
   }
 }
+const jurassicWorldGenres = ['Action', 'Adventure', 'Science Fiction', 'Thriller'];
+ 
+ReactDOM.render(
+  <MovieCard
+    title="Jurassic World"
+    poster="http://image.tmdb.org/t/p/w342/jjBgi2r5cRt36xF6iNUEhzscEcb.jpg"
+    genres={jurassicWorldGenres} 
+  />,
+  document.getElementById('root')
+);
